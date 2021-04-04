92eb7324a9152739d19639b036df410baa60e397
Arrey Tabe <arrey2391@gmail.com>
2021-04-04 13:44:36 +0100

    modifies user.rb file
    adds a users resources

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
-end
+    validates :username, presence:true, uniqueness:true
+    validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+    validates :password, presence:true
