471bdc4e126c2ae71bd42acec548e0c96a263104
Arrey Tabe <arrey2391@gmail.com>
2021-04-19 15:19:12 +0100

    adds unrails helper code for user login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
+
 class User < ApplicationRecord
-    validates :username, presence:true, uniqueness:true
-    validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
-    validates :password, presence:true
+
+	validates :username, presence:true, uniqueness: true
+	validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+	validates :password, presence: true
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  
+
   resources :users, only: %i[new create edit update]
+
+
+ 
+  root 'users#new'
+
+  post 'create' => 'users#create'
+
+
 end
